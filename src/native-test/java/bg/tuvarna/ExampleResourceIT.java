c2f13ab7954659c2e25022783eccf80d31558b6e
tedonikolov <teonikolov01@abv.bg>
2024-12-23 10:12:47 +0200

    Initial commit

---
+++ src/native-test/java/bg/tuvarna/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package bg.tuvarna;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
