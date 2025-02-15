e84b0648ff74f12cce0d77698f18c478fd33ab8e
tedonikolov <teonikolov01@abv.bg>
2025-02-15 11:01:35 +0200

    Fixed config files for server

--- src/main/docker/studentCouncil/build.sh
+++ src/main/docker/studentCouncil/build.sh
@@ -1,4 +1,5 @@
+docker stop $(docker ps -a -q)
 cd ../../../../../student-council-backend
 ./gradlew clean assemble
 cd src/main/docker/studentCouncil
-docker-compose up -d --build
+docker compose up -d --build
